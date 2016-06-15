7c207207e3e8478785d2f245cc313ac03cf52aa0
Jack Noble <jack.carmco@yahoo.com>
2016-06-15 12:45:02 -0500

    Created ruby shell for hamster adoption

---
+++ ruby/hamsters.rb
@@ -0,0 +1,10 @@
+puts "What is this hamster's name?"
+	name = gets.chomp
+puts "How loud is this hamster on a 1-10 scale?"
+	volume = gets.chomp
+puts "What color is the fur?"
+	fur = gets.chomp
+puts "Is this hamster a good candidate for adoption(y/n)?"
+	adoptability = gets.chomp
+puts "What is the hamster's estimated age?"
+	age = gets.chomp
