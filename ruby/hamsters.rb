cb2e08644ad58544bcba650445b51a635829d4d5
Jack Noble <jack.carmco@yahoo.com>
2016-06-15 13:10:47 -0500

    added print to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,19 +1,29 @@
 puts "What is this hamster's name?"
 	name = gets.chomp
 puts "How loud is this hamster on a 1-10 scale?"
-	volume = gets.chomp
-	volume.to_f
+	volume = gets.chomp.to_f
+	puts "#{volume}"
 puts "What color is the fur?"
 	fur = gets.chomp
 puts "Is this hamster a good candidate for adoption(y/n)?"
 	adoptability = gets.chomp
-puts "What is the hamster's estimated age?"
+puts "What is the hamster's estimated age in years?"
 	age = gets.chomp
-	age.to_f
-	if age == ""
+		if age == ""
 		age = nil
 		puts "Age is unknown."
-	else puts "Estimated age is #{age}"
-	end
+		else age = age.to_f
+		end
+puts "Name: #{name}"
+
+puts "Volume(Loudness) on 1-10 scale: #{volume}"
+
+puts "Color fur: #{fur}"
+
+puts "adoptability: #{adoptability}"
+
+puts "Estimated Age: #{age}"
+
+
 
 		
