691f3b2cde7f777d278f22f756ee230a38855e64
Jack Noble <jack.carmco@yahoo.com>
2016-06-19 05:26:53 -0500

    added standard questions to .rb file

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,1 +1,10 @@
 puts "what is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year = gets.chomp
+puts "We have garlic bread on order in our cafeteria, should we order some for you? (yes/no)"
+garlic_bread = gets.chomp
+puts "Would you like to enroll for our health insurance? (yes/no)"
+insurance = gets.chomp
