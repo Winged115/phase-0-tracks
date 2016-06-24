2a180ecba723863e247e7d63ac8cc25718a8a36f
Jack Noble <jack.carmco@yahoo.com>
2016-06-24 16:48:12 -0500

    changed key to used_us_before

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,14 +3,14 @@
 # Add disabilites, salary, have_you_used_us_before.
 # the imput has to be user generated
 # so we will want driving code in the bottom
-# 
+ 
 applicant = {
 	:name =>  "",
 	:number_of_children => "".to_i,
 	:decor_theme => "",
 	:disabilites => "",
 	:salary => "".to_i,
-	:have_you_used_us_before => ""
+	:used_us_before => ""
 }
 
 puts "What is your name?"
@@ -19,12 +19,12 @@ puts "How many children do you have?"
 	applicant[:number_of_children] = gets.chomp
 puts "What is your preferred decor theme?"
 	applicant[:decor_theme] = gets.chomp
-puts "Do you have any disabilites?"
+puts "Do you have any disabilites?(yes/no)"
 	applicant[:disabilites] = gets.chomp
 puts "What is your yearly salary?"
 	applicant[:salary] = gets.chomp
-puts "Have you used us before?"
-	applicant[:have_you_used_us_before] = gets.chomp
+puts "Have you used us before?(yes/no)"
+	applicant[:used_us_before] = gets.chomp
 
 p applicant
 
@@ -37,3 +37,31 @@ end
 # the user back to the category they want to change.
 # If no change is wanted, they type "none", and we continue 
 # on to the rest of the program.
+puts "Are there any categories you would like to change?Please enter the category or type 'none'"
+	user_input = gets.chomp
+		if user_input == "name"
+			puts "Name:"
+			applicant[:name] = gets.chomp
+		elsif user_input == "number_of_children"
+			puts "Number of Children:"
+			applicant[:number_of_children] = gets.chomp
+		elsif user_input == "decor_theme"
+			puts "Decor Theme:"
+			applicant[:decor_theme] = gets.chomp
+		elsif user_input == "disabilites"
+			puts "Disabilites:"
+			applicant[:disabilites] = gets.chomp
+		elsif user_input == "salary"
+			puts "Salary:"
+			applicant[:salary] = gets.chomp
+		elsif user_input == "used_us_before"
+
+
+
+
+
+
+
+
+
+			
