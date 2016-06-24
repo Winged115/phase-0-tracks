164ba19e427217a848b605b77ea5139b02ec97af
Jack Noble <jack.carmco@yahoo.com>
2016-06-24 15:51:04 -0500

    initialized, started psuedocoding

---
+++ ruby/hashes.rb
@@ -0,0 +1,5 @@
+# First create a hash with the appropriate data types
+# name, age, number of children, decor theme. Add a few of my own
+# Add disabilites, salary, have_you_used_us_before.
+# the imput has to be user generated
+# so we will want driving code in the bottom
