d394c03bd7782747f400a96e4b9e3e806ffc2bf8
Jack Noble <jack.carmco@yahoo.com>
2016-06-26 01:37:45 -0500

    fixed syntax errors, tested out first nested data call

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,54 +4,55 @@
 # within our nested data. OK, now think of some data you jackinape!
 
 michigan = {
-	metro_detroit: {
+	metro_detroit:{
 		population: "2000000".to_i,
-			ethnicity: {
+			ethnicity:{
 				white: "70%",
 				black: "23%",
 				hispanic: "6%",
 				arab: "5%"
-			}
-				towns: [
-					"Birmingham",
-					"Troy",
-					"West Bloomfield",
-					"Southfield",
-					"Royal Oak",
-					"Dearborn"
-				]
-	}
-	detroit: {
+			},
+				town: [
+						"Birmingham",
+						"Troy",
+						"West Bloomfield",
+						"Southfield",
+						"Royal Oak",
+						"Dearborn"
+					]
+	},
+	detroit:{
 		population: "720000".to_i,
-			ethnicity: {
+			ethnicity:{
 				white: "11%",
 				black: "83%",
 				hispanic: "7%",
 				arab: "5%"
-			}
-				district: [
+			},
+				district:[
 					"Midtown",
 					"Jefferson Corridor",
 					"Corktown",
 					"Riverfront",
 					"Southwest Detroit"
 				]
-	upper_peninsula: {
+	},
+	upper_peninsula:{
 		population: "311000".to_i,
-			ethnicity: {
+			ethnicity:{
 				white: "94%",
 				black: "2%",
 				hispanic: ".4%",
 				asian: ".7%"
-
-			}
-				towns: [
+			},
+				town:[
 					"Marquette",
 					"Ironwood",
 					"Escanaba",
 					"St. Ignace",
 					"Ishpeming"
 				]
-	}
+	},
 }
-}
+
+p michigan[:metro_detroit][:town]
