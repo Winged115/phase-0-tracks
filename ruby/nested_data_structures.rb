e6b38f8d26983be732052e76b760cceb9a1f11bd
Jack Noble <jack.carmco@yahoo.com>
2016-06-26 00:48:55 -0500

    added first data to hash 'michigan'

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,2 +1,17 @@
 # Think of a real world example of data within data
-# within data.
+# within data. Remember we have to call this data in certain ways.
+# Also, we should be thinking of ways to push and modify certain data sets
+# within our nested data. OK, now think of some data you jackinape!
+
+michigan = {
+	suburban_detroit: {
+		town_names: ["Troy",
+					 "Birmingham",
+					 "Southfield",
+					 "Royal Oak",
+					 "West Bloomfield"
+		],
+	}
+
+}
+p michigan[:suburban_detroit][:town_names]
