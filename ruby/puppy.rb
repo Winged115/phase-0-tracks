2ffa52bb1ce725339c621fddb616add76e1db0b7
Jack Noble <jack.carmco@yahoo.com>
2016-06-28 16:00:24 -0500

    Initialized and added characteristics and behaviors to class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# SPECIES--------------
+#
+# Canus Lupus
+#
+# CHARACTERISTICS------
+#
+# Tail Length: Varies
+# Friendly: yes
+# Eye Count: 2
+# Name: Varies
+# Color: Varies
+# Bark Volume: Varies
+#
+# BEHAVIOR-------------
+#
+# Wag Tail
+# Run
+# Bark
+# Cuddle
+# Sleep
+# 
