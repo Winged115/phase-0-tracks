2d10e0e6c91300535fd61e14c3099a669ad3eddf
Jack Noble <jack.carmco@yahoo.com>
2016-06-29 15:26:44 -0500

    commented out previous calls, working on adding new santas to array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,11 +22,8 @@ end
 #santa.speak
 #santa.eat_milk_and_cookies("Chocolate Chip Cookie")
 santas =[]
+ 
 
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+
+
+
