70c114d1f2025b64184cfc353a78b46311daf03b
Jack Noble <jack.carmco@yahoo.com>
2016-06-29 15:17:16 -0500

    added attributes to santa class, started empty santas array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance..."
 	end
 
@@ -14,6 +18,15 @@ class Santa
 
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("Chocolate Chip Cookie")
+#santa = Santa.new
+#santa.speak
+#santa.eat_milk_and_cookies("Chocolate Chip Cookie")
+santas =[]
+
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
