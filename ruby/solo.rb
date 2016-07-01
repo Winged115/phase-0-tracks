88868c07348a9903b150db07d6ca6bad64e88711
Jack Noble <jack.carmco@yahoo.com>
2016-06-30 21:51:09 -0500

    Pushed my data into an array for initialzing new monsters.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -25,7 +25,7 @@ class Sea_Monster
 	attr_accessor :name
 
 
-	def initialize(name, location)
+	def initialize(name, location, age, speed)
 		@name = name
 		@location = location
 		@age = age
@@ -55,6 +55,7 @@ end
 # p "the new name is #{monster.name}."
 # p "The #{monster.name} is #{monster.speed}"
 monsters = []
+
 puts "Name?"
 name = gets.chomp
 puts "Location?"
@@ -65,5 +66,9 @@ puts "Speed?"
 speed = gets.chomp
 
 
-monsters << Sea_Monster.new(name, location)
-p monsters
+monsters << Sea_Monster.new(name, location, age, speed)
+p monsters[0].name
+p monsters[0].location
+p monsters[0].age
+p monsters[0].speed
+monsters[0].roar
