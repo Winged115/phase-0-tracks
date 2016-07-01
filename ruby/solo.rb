190283b8b441dc9bbdc70fcf2bc370b5ff155ed6
Jack Noble <jack.carmco@yahoo.com>
2016-06-30 21:34:22 -0500

    deleted all driver code, it wasn't going anywhere

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -25,11 +25,11 @@ class Sea_Monster
 	attr_accessor :name
 
 
-	def initialize(name, location)
+	def initialize(name, location, age, speed)
 		@name = name
 		@location = location
-		@age = rand(100..5000)
-		@speed = "Faster than anything I've ever seen."
+		@age = age
+		@speed = speed
 		puts "Initializing new Sea Monster instance..."
 	end
 
@@ -38,11 +38,11 @@ class Sea_Monster
 	end
 
 	def destroy(ship_type)
-		puts "*The #{@name} rips through the #{ship_type}*"
+		puts "*#{@name} rips through the #{ship_type}*"
 	end
 
 	def release
-		puts "Release THE #{@name}!"
+		puts "Release #{@name}!"
 	end
 end
 
@@ -55,3 +55,41 @@ end
 # p "the new name is #{monster.name}."
 # p "The #{monster.name} is #{monster.speed}"
 monsters = []
+
+name = []
+
+location = []
+
+age = []
+
+speed = []
+valid_input = false
+puts "We need information on Sea Monsters, when you are done type 'exit'"
+puts "------"
+until valid_input
+	puts "What is the creature's name?"
+	name << gets.chomp
+	
+	puts "Where is the beast located?"
+	location << gets.chomp
+	
+	puts "How old is this primordial beast?"
+	age << gets.chomp.to_i
+	
+	puts "How fast is the monster?"
+	speed << gets.chomp
+	
+	monsters <<Sea_Monster.new(name,location, age, speed)
+
+	puts "If finished type 'exit', otherwise type 'new monster'."
+		user_input = gets.chomp
+			if user_input == "exit"
+				valid_input = true
+			else user_input != "exit"
+			end
+end
+
+p monsters.name
+p monsters.location
+p monsters.age
+p monsters.speed
