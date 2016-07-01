9c62a77bd4a4ef422bd9a97e5683aba48b6564a5
Jack Noble <jack.carmco@yahoo.com>
2016-06-30 20:18:32 -0500

    started class outline 'Leviathan'

---
+++ ruby/solo.rb
@@ -0,0 +1,13 @@
+# CLASS--------------------
+#
+# Leviathan
+#
+#ATTRIBUTES----------------
+#
+# Type of monster: Kraken, Jaws, Cthulu, Hydra, Leviathan.
+#
+# Age: 100-5000
+# 
+# Speed: 
+#
+# Location: 
