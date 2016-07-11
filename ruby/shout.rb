f2f4e3b67d34c04e6ba941c0499fc42963d95af7
Jack Noble <jack.carmco@yahoo.com>
2016-07-10 22:16:31 -0500

    commented out old code, making new mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,30 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + ":("
-	end
 
-	def self.yelling_happily(words)
-		words + " and rainbows!!!" + " :D"
-	end
-end
 
-p Shout.yell_angrily('This is hard sometimes')
 
-p Shout.yelling_happily('But I feel really accomplished when it works')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + ":("
+# 	end
+# 
+# 	def self.yelling_happily(words)
+# 		words + " and rainbows!!!" + " :D"
+# 	end
+# end
+# 
+# p Shout.yell_angrily('This is hard sometimes')
+# 
+# p Shout.yelling_happily('But I feel really accomplished when it works')
+
