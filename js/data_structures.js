92851394753490543d69c18788b2719a2c127b5d
Jack Noble <jack.carmco@yahoo.com>
2016-07-15 14:01:34 -0500

    added push methods to var names

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,11 @@
 var colors = ['blue','green','yellow','red'];
 
-var names = ['Bullseye', 'Ed', 'York', 'American']
+var names = ['Bullseye', 'Ed', 'York', 'American'];
+
+colors.push('orange');
+
+names.push('Atreyu');
+
+console.log(colors)
+
+console.log(names)
