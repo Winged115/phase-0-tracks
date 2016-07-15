faa895bb798727f69fd885b29ef3bf7566516ba5
Jack Noble <jack.carmco@yahoo.com>
2016-07-15 15:00:50 -0500

    finished releases 2-3

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,34 @@ for (var i = 0; i < colors.length; i++) {
 
 
 console.log(horse);
+
+//console.log(horse[names['Bullseye']]);
+
+function Car(brand, topspeed, color) {
+	// In this context, 'this' refers to 
+	// the individual dog we're 
+	console.log("Our new car:", this);
+	
+	// Therefore, this.name is sort of the Ruby
+	// equivalent of @name
+	this.brand = brand;
+	this.topspeed = topspeed;
+	this.color = color;
+	
+	// As for behavior, a function is a perfectly 
+	// valid property value ... whoa!
+	this.revv = function() { console.log("Vroom!"); };
+	
+	console.log("CAR INITIALIZATION COMPLETE");
+
+}
+
+var newcar = new Car('Ford', 180, 'purple');
+
+console.log(newcar);
+
+newcar.revv();
+
+var newcar2 = new Car('Dodge', 200, 'red');
+
+var newcar3 = new Car('BMW', 240, 'blue');
