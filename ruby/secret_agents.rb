d55794113c7deff995191f5b4a251ad0d45492ad
Jack Noble <jack.carmco@yahoo.com>
2016-07-15 19:09:05 -0500

    changed encrypt method to better solution

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,50 +1,19 @@
-	def encrypt (msg)
-    	count = 0
-    	newMsg = ""
-    	while count < msg.length
-    	 msg_to_int = (msg[count].ord + 1)
-    	 if msg_to_int > 122
-            msg_to_int = 97
-        end
-        newMsg += msg_to_int.chr
-        count+=1
+
+def encrypt(str)
+    new_str = ''
+
+    index = 0
+    while index < str.length
+        if str[index] == 'z'
+            new_str += 'a'
+        elsif str[index] == ' '
+            new_str += ' '
+        else
+            new_str += str[index].next
+        end 
+    index += 1
     end
-    return newMsg
-	end
-
-	def decrypt (msg)
-    	count = 0
-    	newMsg = ""
-    	while count < msg.length
-        	msg_to_int = (msg[count].ord - 1)
-        	if     msg_to_int < 97
-            msg_to_int = 122
-        end
-        newMsg += msg_to_int.chr
-        count+=1
-        end
-    return newMsg
-	end
-
-
-#Driver code
-valid_input = false
-until valid_input
-	puts "Hello agent, would you like to encrypt or decrypt?"
-	preference = gets.chomp
-	if preference == "encrypt"
-		puts "What is your password for encryption?"
-		msg = gets.chomp
-		valid_input = true
-		result = encrypt(msg)
-		puts encrypt(msg)
-	elsif preference == "decrypt"
-		puts "what is your password for decryption?"
-		msg = gets.chomp
-		valid_input = true
-		result = decrypt(msg)
-		puts decrypt(msg)
-	else puts "I'm sorry That did not compute."
+    p new_str
 end
-end
-p result
+
+encrypt('x y z')
