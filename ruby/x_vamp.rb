94f91c3cc2e478eeb51589d739edd93d13d4da91
Jack Noble <jack.carmco@yahoo.com>
2016-07-15 20:46:28 -0500

    added questions to vampires logic

--- ruby/x_vamp.rb
+++ ruby/x_vamp.rb
@@ -1,4 +1,21 @@
 puts 'What is your name?'
 
+name = gets.chomp
+
+puts 'How old are you?'
+
+age = gets.chomp.to_i
+
+puts 'When were you born?'
 
 
+birth_year = gets.chomp.to_i
+
+
+puts 'We serve garlic bread here, should we order some for you?'
+
+galric_pref = gets.chomp
+
+puts 'Would you like to enroll in our company PPO health insurance?'
+
+insurance_pref = gets.chomp
