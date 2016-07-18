c8b41366cf2dc7c0cf98b838291c84e2ad1da873
Jack Noble <jack.carmco@yahoo.com>
2016-07-18 01:26:48 -0500

    initialized algos file, added psuedocode about sorting an array of strings

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+//Function input: array of strings
+//output: array of same strings
+//what it does: sorts array of strings from longest to shortest
+//
+//use js languague to sort strings within an array
+
