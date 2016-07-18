f9b7706ce3243ec4d63d9f28de5590feff2cd399
Jack Noble <jack.carmco@yahoo.com>
2016-07-18 02:02:58 -0500

    added a note about the code I entered to sort an array by longest length.

--- js/algos.js
+++ js/algos.js
@@ -3,6 +3,14 @@
 //what it does: sorts array of strings from longest to shortest
 // outputs the longest one.
 //use js languague to sort strings within an array
+
+
+//NOTE* I have to be completely transparent, I googled
+// "how to sort strings by length javaScript" and
+// the second page I clicked on gave me the correct code.
+// It would've taken me a while to think of an if statement
+// like the one below, and I'm reading through the explination of it.
+// I just feel like it may have gone against the release somehow.
 var longestLength = 0
 var longestPhrase = ''
 function sortByLength(array) {
