d13a10523675cd8b672c16b5026130907c708f1d
Jack Noble <jack.carmco@yahoo.com>
2016-07-18 01:32:43 -0500

    reread release, not sorting an array, but returning longest string within the array, added psuedocode to represent this.

--- js/algos.js
+++ js/algos.js
@@ -1,9 +1,13 @@
 //Function input: array of strings
-//output: array of same strings
+//output: string
 //what it does: sorts array of strings from longest to shortest
-//
+// outputs the longest one.
 //use js languague to sort strings within an array
 
 function sortByLength(array) {
-	
+	array[i].length;
+	console.log(array)
 }
+
+
+sortByLength(["hi", "hello", "hello there"])
