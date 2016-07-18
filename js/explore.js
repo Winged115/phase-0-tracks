a5b053d12915d35fa02b5ab3b3bc6d1d06692214
Jack Noble <jack.carmco@yahoo.com>
2016-07-18 00:39:39 -0500

    added some psuedocoding

---
+++ js/explore.js
@@ -0,0 +1,4 @@
+// We want to put in a string and have the last object in the string
+// become the first object in the string, and so on and so forth.
+// there is no reverse method, so we want to build a function that
+// can deconstruct our string then reassemble it from the back to front.
