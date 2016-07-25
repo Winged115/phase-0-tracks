21923fd4361daa5df7b5f61706d5015804baf501
Jack Noble <jack.carmco@yahoo.com>
2016-07-25 04:44:14 -0500

    create database in ruby with delimiters for tables

--- self_eval/evaluation.rb
+++ self_eval/evaluation.rb
@@ -1,2 +1,20 @@
 require 'sqlite3'
 
+db = SQlite3::Database.new("productive.db")
+
+create_table1_cmd = <<- SQl
+	CREATE TABLE IF NOT EXISTS procrastination(
+		id INTEGER PRIMARY KEY,
+		issue VARCHAR(255),
+		rating INT
+		)
+SQL
+
+
+create_table2_cmd = <<- SQL
+	CREATE TABLE IF NOT EXISTS solution(
+		id INTEGER PRIMARY KEY,
+		solution VARCHAR(255)
+		)
+SQL
+
